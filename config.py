23acc90ac26cdace77891b616e8da22a3cb557b7
unknown <imanajetna20@gmail.com>
2026-02-11 18:17:14 +0100

    Initial commit - bot Telegram prêt pour deployment

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")  # récupérer depuis Render secrets
 FOLDER_ID = os.getenv("FOLDER_ID")  # ID du dossier Google Drive
-SERVICE_ACCOUNT_FILE = "credentialsjson"
+SERVICE_ACCOUNT_FILE = "credentials.json"
